b048ea6fb67f41eb3dca6724fc4cf7ebe059b94f
weiwang <weiwang@redhat.com>
2016-09-02 09:47:18 +0800

    make test file for subscription functions

---
+++ pages/terminal_page.py
@@ -0,0 +1,14 @@
+from utils.page_objects import PageObject, PageElement
+
+class TerminalPage(PageObject):
+    """Execute shell command in the terminal"""
+
+    # Elements
+
+    def __init__(self, *args, **kwargs):
+        super(TerminalPage, self).__init__(*args, **kwargs)
+        self.get("/system/terminal")
+
+    # function_1: execute command
+    def execute_command(self):
+        pass
---
+++ tests/test_subscriptions_functions.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from pages.login_page import LoginPage
+from pages.subscriptions_page import SubscriptionsPage
+
+
+ROOT_URI = "https://10.66.11.209:9090"
+
+
+@pytest.fixture(scope="module")
+def firfox(request):
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(20)
+    root_uri = getattr(request.module, "ROOT_URI", None)
+    driver.root_uri = root_uri
+    yield driver
+    driver.close()
+
+
+def test_login_page(firfox):
+    login_page = LoginPage(firfox)
+    login_page.basic_check_elements_exists()
+    login_page.login_with_credential()
+
+
+def test_rhsm_page(firfox):
+    subscriptions_page = SubscriptionsPage(firfox)
+    subscriptions_page.basic_check_elements_exists()
+    subscriptions_page.register_rhsm()
+    subscriptions_page.check_subscription_result()
+    # subscriptions_page.unregister_subsciption()
+
+
+# def test_rhsm_keyOrg_page(firfox):
+#     subscriptions_page = SubscriptionsPage(firfox)
+#     subscriptions_page.basic_check_elements_exists()
+#     subscriptions_page.register_rhsm_key_org()
+#     subscriptions_page.check_subscription_result()
+#     # subscriptions_page.unregister_subsciption()
