22c2be17552d5bdf8147cf00af3e275927eb2107
yaniwang <yaniwang@redhat.com>
2016-09-29 15:13:26 +0800

    add 2 rhevm helper function 1. find if host already registed to rhevm 2. remove a registed host by host_name

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import base64
+import time
 import requests
 import shutil
 
@@ -14,7 +15,7 @@ class RhevmAction:
     api_url = "https://{rhevm_fqdn}/ovirt-engine/api/{item}"
 
     headers = {"Prefer": "persistent-auth",
-               "Accept": "application/xml",
+               "Accept": "application/json",
                "Content-type": "application/xml"}
 
     cert_url = "https://{rhevm_fqdn}/ovirt-engine/services/pki-resource?resource=ca-certificate&format=X509-PEM-CA"
@@ -55,6 +56,33 @@ class RhevmAction:
         else:
             raise RuntimeError("Can not get the cert file from %s" % self.rhevm_fqdn)
 
+    def query_host_id_by_name(self, host_name):
+        api_url = self.api_url.format(rhevm_fqdn=self.rhevm_fqdn, item="hosts")
+
+        r = self.req.get(api_url,
+                         headers=self.headers,
+                         verify=self.rhevm_cert)
+        if r.status_code != 200:
+            raise RuntimeError("Can not list hosts from %s" % self.rhevm_fqdn)
+
+        hosts = r.json()
+        for host in hosts['host']:
+            if host['name'] == host_name:
+                return host['id']
+        return False
+
+    def _deactive_host(self, host_id):
+        api_url_base = self.api_url.format(rhevm_fqdn=self.rhevm_fqdn, item='hosts')
+        api_url = api_url_base + "/%s/deactivate" % host_id
+        # print api_url
+        r = self.req.post(api_url,
+                          headers=self.headers,
+                          verify=self.rhevm_cert,
+                          data="<action/>")
+        ret = r.json()
+        if ret['status'] != 'complete':
+            raise RuntimeError(ret['fault']['detail'])
+
     def add_new_host(self, *rhvh_credentials):
         ip, host_name, password = rhvh_credentials
         api_url = self.api_url.format(rhevm_fqdn=self.rhevm_fqdn, item="hosts")
@@ -71,8 +99,20 @@ class RhevmAction:
             print r.text
             raise RuntimeError("Failed to add new host, may be host already imported")
 
-    def remove_host(self):
-        raise NotImplementedError
+    def remove_host(self, host_name):
+        api_url_base = self.api_url.format(rhevm_fqdn=self.rhevm_fqdn, item="hosts")
+        host_id = self.query_host_id_by_name(host_name)
+
+        if host_id:
+            self._deactive_host(host_id)
+            time.sleep(5)
+            api_url = api_url_base + '/%s' % host_id
+
+            r = self.req.delete(api_url, headers=self.headers, verify=self.rhevm_cert)
+            if r.status_code != 200:
+                raise RuntimeError("Can not delete host %s" % host_name)
+        else:
+            print "Can't find host with name %s" % host_name
 
 
 if __name__ == '__main__':
